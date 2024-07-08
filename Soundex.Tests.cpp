06fd4b3b83a2afd9967e3bf4688e7757dca93d55
sakshi0210 <73066338+sakshi0210@users.noreply.github.com>
2024-07-08 16:12:38 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -3,7 +3,54 @@
 
 
 TEST(SoundexTest, HandlesEmptyString) {
-    EXPECT_EQ(generateSoundex(""), "");
+    EXPECT_EQ(generateSound#include <gtest/gtest.h>
+#include "Soundex.h"
+
+TEST(SoundexTest, HandlesEmptyString) {
+    EXPECT_EQ(generateSoundex(""), "0000");
+}
+
+TEST(SoundexTest, HandlesSingleCharacter) {
+    EXPECT_EQ(generateSoundex("A"), "A000");
+}
+
+TEST(SoundexTest, BasicFunctionality) {
+    EXPECT_EQ(generateSoundex("Smith"), "S530");
+    EXPECT_EQ(generateSoundex("Smythe"), "S530");
+}
+
+TEST(SoundexTest, CaseInsensitivity) {
+    EXPECT_EQ(generateSoundex("smith"), "S530");
+    EXPECT_EQ(generateSoundex("SMITH"), "S530");
+}
+
+TEST(SoundexTest, AdjacentSimilarSoundingConsonants) {
+    EXPECT_EQ(generateSoundex("Pfister"), "P236");
+    EXPECT_EQ(generateSoundex("Honeyman"), "H555");
+}
+
+TEST(SoundexTest, VowelsAndIgnoredLetters) {
+    EXPECT_EQ(generateSoundex("Ashcraft"), "A261");
+    EXPECT_EQ(generateSoundex("Tymczak"), "T522");
+}
+
+TEST(SoundexTest, EdgeCases) {
+    EXPECT_EQ(generateSoundex("A"), "A000");
+    EXPECT_EQ(generateSoundex(""), "0000");
+}
+
+TEST(SoundexTest, RepeatedLettersWithHOrWInBetween) {
+    EXPECT_EQ(generateSoundex("Ashcraft"), "A261");
+    EXPECT_EQ(generateSoundex("Tymczak"), "T522");
+    EXPECT_EQ(generateSoundex("Ashworth"), "A263");
+    EXPECT_EQ(generateSoundex("Jankowski"), "J520");
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+ex(""), "");
 }
 
 TEST(SoundexTest, HandlesSingleCharacter) {
