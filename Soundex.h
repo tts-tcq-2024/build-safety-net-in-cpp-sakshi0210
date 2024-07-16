604f12f463f66b93c94eeece56f6bfc4f3b6ef2c
sakshi0210 <73066338+sakshi0210@users.noreply.github.com>
2024-07-16 14:40:23 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -3,8 +3,17 @@
 
 #include <string>
 
-std::string generateSoundex(const std::string& name);
-char getSoundexCode(char c);
-bool isVowelOrIgnored(char c);
+// Function to get the soundex code for a given character
+char get_soundex_code(char c);
+
+// Function to compare characters and return the soundex code if conditions are met
+std::string comparison(char ch, char prev_code);
+
+// Function to map the rest of the name to soundex codes
+std::string num_map(const std::string& name, char& prev_code);
+
+// Function to generate the soundex code for a given name
+std::string generate_soundex(const std::string& name);
 
 #endif // SOUNDEX_H
+
