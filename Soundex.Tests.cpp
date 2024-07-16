14ea57e0e77e0120a2e2922222b670a9abea32b2
sakshi0210 <73066338+sakshi0210@users.noreply.github.com>
2024-07-16 14:41:47 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,44 +1,23 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
 
-TEST(SoundexTest, HandlesEmptyString) {
-    EXPECT_EQ(generateSoundex(""), "0000");
+// Test cases for generate_soundex function
+
+TEST(SoundexTest, EmptyString) {
+    EXPECT_EQ(generate_soundex(""), "");
 }
 
-TEST(SoundexTest, HandlesSingleCharacter) {
-    EXPECT_EQ(generateSoundex("A"), "A000");
+TEST(SoundexTest, SingleCharacter) {
+    EXPECT_EQ(generate_soundex("A"), "A000");
 }
 
-TEST(SoundexTest, BasicFunctionality) {
-    EXPECT_EQ(generateSoundex("Smith"), "S530");
-    EXPECT_EQ(generateSoundex("Smythe"), "S530");
-}
-
-TEST(SoundexTest, CaseInsensitivity) {
-    EXPECT_EQ(generateSoundex("smith"), "S530");
-    EXPECT_EQ(generateSoundex("SMITH"), "S530");
-}
-
-TEST(SoundexTest, AdjacentSimilarSoundingConsonants) {
-    EXPECT_EQ(generateSoundex("Pfister"), "P123");
-    EXPECT_EQ(generateSoundex("Honeyman"), "H555");
-}
-
-TEST(SoundexTest, VowelsAndIgnoredLetters) {
-    EXPECT_EQ(generateSoundex("Ashcraft"), "A261");
-    EXPECT_EQ(generateSoundex("Tymczak"), "T522");
-}
-
-TEST(SoundexTest, EdgeCases) {
-    EXPECT_EQ(generateSoundex("A"), "A000");
-    EXPECT_EQ(generateSoundex(""), "0000");
-}
-
-TEST(SoundexTest, RepeatedLettersWithHOrWInBetween) {
-    EXPECT_EQ(generateSoundex("Ashcraft"), "A261");
-    EXPECT_EQ(generateSoundex("Tymczak"), "T522");
-    EXPECT_EQ(generateSoundex("Ashworth"), "A263");
-    EXPECT_EQ(generateSoundex("Jankowski"), "J520");
+TEST(SoundexTest, SimpleCases) {
+    EXPECT_EQ(generate_soundex("Robert"), "R163");
+    EXPECT_EQ(generate_soundex("Rupert"), "R163");
+    EXPECT_EQ(generate_soundex("Rubin"), "R150");
+    EXPECT_EQ(generate_soundex("Ashcraft"), "A261");
+    EXPECT_EQ(generate_soundex("Ashcroft"), "A261");
+    EXPECT_EQ(generate_soundex("Pfister"), "P236");
 }
 
 int main(int argc, char **argv) {
