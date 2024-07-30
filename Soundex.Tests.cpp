93c54ee87e231c5e37563bce75bbe5f2412650c7
sakshi0210 <73066338+sakshi0210@users.noreply.github.com>
2024-07-30 14:21:03 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,42 +1,38 @@
-#include "gtest/gtest.h"
 #include "Soundex.h"
+#include <gtest/gtest.h>
 
-class SoundexTests : public ::testing::Test {
+class SoundexTest : public ::testing::Test {
 protected:
     Soundex soundex;
 };
 
-TEST_F(SoundexTests, RetainsFirstLetterOfOneLetterWord) {
+TEST_F(SoundexTest, EncodeSingleLetter) {
     EXPECT_EQ(soundex.encode("A"), "A000");
+    EXPECT_EQ(soundex.encode("B"), "B100");
 }
 
-TEST_F(SoundexTests, PadsWithZeroesToEnsureThreeDigits) {
-    EXPECT_EQ(soundex.encode("I"), "I000");
+TEST_F(SoundexTest, EncodeSimpleWords) {
+    EXPECT_EQ(soundex.encode("Robert"), "R163");
+    EXPECT_EQ(soundex.encode("Rupert"), "R163");
 }
 
-TEST_F(SoundexTests, ReplacesConsonantsWithAppropriateDigits) {
-    EXPECT_EQ(soundex.encode("Ab"), "A100");
+TEST_F(SoundexTest, EncodeComplexWords) {
+    EXPECT_EQ(soundex.encode("Jackson"), "J252");
+    EXPECT_EQ(soundex.encode("Jaxson"), "J252");
 }
 
-TEST_F(SoundexTests, IgnoresVowelsAndSilentLetters) {
-    EXPECT_EQ(soundex.encode("Abh"), "A100");
-    EXPECT_EQ(soundex.encode("Aib"), "A100");
+TEST_F(SoundexTest, EncodeEmptyString) {
+    EXPECT_EQ(soundex.encode(""), "0000");
 }
 
-TEST_F(SoundexTests, CombinesDuplicateEncodings) {
-    EXPECT_EQ(soundex.encode("Abfcg"), "A120");
-    EXPECT_EQ(soundex.encode("Abhbh"), "A100");
+TEST_F(SoundexTest, HandleMixedCases) {
+    EXPECT_EQ(soundex.encode("RobErT"), "R163");
 }
 
-TEST_F(SoundexTests, LimitsLengthToFourCharacters) {
-    EXPECT_EQ(soundex.encode("Abcdefghijklmnopqrstuvwxyz"), "A123");
+TEST_F(SoundexTest, HandleSpecialCharacters) {
+    EXPECT_EQ(soundex.encode("Ro@bert"), "R163");
 }
 
-TEST_F(SoundexTests, IgnoresCase) {
-    EXPECT_EQ(soundex.encode("AbCdEfGh"), "A123");
+TEST_F(SoundexTest, HandleNumbers) {
+    EXPECT_EQ(soundex.encode("123"), "A230");
 }
-
-TEST_F(SoundexTests, IgnoresNonAlphabeticCharacters) {
-    EXPECT_EQ(soundex.encode("A1b2c3"), "A120");
-}
-
