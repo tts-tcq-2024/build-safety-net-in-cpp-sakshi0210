226b108e5045030fb4fc703137d0340ed03e1bd0
sakshi0210 <73066338+sakshi0210@users.noreply.github.com>
2024-07-30 14:11:55 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -2,7 +2,19 @@
 #define SOUNDEX_H
 
 #include <string>
+#include <unordered_map>
 
-std::string generateSoundex(const std::string& name);
+class Soundex {
+public:
+    std::string encode(const std::string& word) const;
+
+private:
+    std::string mapToDigit(char letter) const;
+    std::string removeDuplicateDigits(const std::string& encoding) const;
+    std::string padToFourCharacters(const std::string& encoding) const;
+
+    static const std::unordered_map<char, char> charToDigit;
+};
 
 #endif // SOUNDEX_H
+
