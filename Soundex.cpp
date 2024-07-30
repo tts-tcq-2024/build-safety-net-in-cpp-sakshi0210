f9ff31c9b8bf198b81e735775be82602081c6927
sakshi0210 <73066338+sakshi0210@users.noreply.github.com>
2024-07-30 14:20:14 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -1,5 +1,6 @@
 #include "Soundex.h"
 #include <cctype>
+#include <unordered_map>
 
 const std::unordered_map<char, char> Soundex::charToDigit = {
     {'b', '1'}, {'f', '1'}, {'p', '1'}, {'v', '1'},
@@ -13,34 +14,37 @@ const std::unordered_map<char, char> Soundex::charToDigit = {
 std::string Soundex::encode(const std::string& word) const {
     if (word.empty()) return "0000";
 
-    std::string encoding(1, std::toupper(word.front()));
+    std::string encoding;
+    encoding += std::toupper(word[0]); // Retain the first letter
 
+    char prevDigit = '0';
     for (size_t i = 1; i < word.size(); ++i) {
         char letter = std::tolower(word[i]);
-        if (charToDigit.count(letter) && (encoding.empty() || encoding.back() != charToDigit.at(letter))) {
-            encoding += charToDigit.at(letter);
+        char digit = mapToDigit(letter);
+
+        if (digit != prevDigit && digit != '0') {
+            encoding += digit;
+            prevDigit = digit;
         }
     }
 
     return padToFourCharacters(encoding);
 }
 
-std::string Soundex::mapToDigit(char letter) const {
-    auto it = charToDigit.find(std::tolower(letter));
-    return it != charToDigit.end() ? std::string(1, it->second) : "";
-}
-
-std::string Soundex::removeDuplicateDigits(const std::string& encoding) const {
-    std::string result;
-    for (char digit : encoding) {
-        if (result.empty() || result.back() != digit) {
-            result += digit;
-        }
-    }
-    return result;
+char Soundex::mapToDigit(char letter) const {
+    auto it = charToDigit.find(letter);
+    return it != charToDigit.end() ? it->second : '0';
 }
 
 std::string Soundex::padToFourCharacters(const std::string& encoding) const {
-    return encoding.substr(0, 1) + removeDuplicateDigits(encoding.substr(1)).substr(0, 3) + std::string(4 - encoding.size(), '0');
+    std::string result = encoding.substr(0, 1);
+    for (size_t i = 1; i < encoding.size(); ++i) {
+        if (result.size() < 4) {
+            result += encoding[i];
+        }
+    }
+    while (result.size() < 4) {
+        result += '0';
+    }
+    return result;
 }
-
