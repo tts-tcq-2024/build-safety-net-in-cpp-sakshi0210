89eb7d5664504af78abd908420bb91b0ad5be3b7
sakshi0210 <73066338+sakshi0210@users.noreply.github.com>
2024-08-06 16:54:07 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -1,31 +1,58 @@
 #include "Soundex.h"
+#include <unordered_map>
+#include <cctype>
+#include <string>
+
+static const std::unordered_map<char, char> soundexMap = {
+        {'B', '1'}, {'F', '1'}, {'P', '1'}, {'V', '1'},
+        {'C', '2'}, {'G', '2'}, {'J', '2'}, {'K', '2'},
+        {'Q', '2'}, {'S', '2'}, {'X', '2'}, {'Z', '2'},
+        {'D', '3'}, {'T', '3'},
+        {'L', '4'},
+        {'M', '5'}, {'N', '5'},
+        {'R', '6'}
+};
 
 char getSoundexCode(char c) {
-    static const char codeTable[26] = {
-        '0', '1', '2', '3', '0', '1', '2', '0', '0', '2', '2', '4', '5', '5', '0', '1', '2', '6', '2', '3', '0', '1', '0', '2', '0', '2'
-    };
-    c = toupper(c);
-    if (isalpha(c)) {
-        return codeTable[c - 'A'];
-    }
-    return '0';
+    c = std::toupper(c);
+    auto it = soundexMap.find(c);
+    return (it != soundexMap.end()) ? it->second : '0';
 }
 
-int updateSoundexCode(char code, int sIndex, char *soundex) {
-    if (code != '0') {
-        soundex[sIndex] = code;
-        return ++sIndex;
-    }
-    return sIndex;
+bool isCodeValid(char code, char prevCode) {
+    return code != '0' && code != prevCode;
 }
-
-void generateSoundexCode(const char *name, char *soundex) {
-    soundex[0] = toupper(name[0]);
-    int sIndex = 1;
-    for (int i = 1; name[i] != '\0' && sIndex <= 3; i++) {
+ 
+bool isSoundexLengthValid(const std::string& soundex) {
+    return soundex.length() < 4;
+}
+ 
+std::pair<std::string, char> addSoundexCode(const std::string& soundex, char code, char prevCode) {
+    std::string newSoundex = soundex;
+    if (isCodeValid(code, prevCode) && isSoundexLengthValid(soundex)) {
+        newSoundex += code;
+        prevCode = code;
+    }
+    return {newSoundex, prevCode};
+}
+ 
+std::string updateSoundex(const std::string& soundex, const std::string& name, char prevCode) {
+    std::string newSoundex = soundex;
+    for (size_t i = 1; i < name.size(); ++i) {
         char code = getSoundexCode(name[i]);
-        sIndex = updateSoundexCode(code, sIndex, soundex);
+        auto result = addSoundexCode(newSoundex, code, prevCode);
+        newSoundex = result.first;
+        prevCode = result.second;
     }
-    memset(soundex + sIndex, '0', 4 - sIndex);
-    soundex[4] = '\0';
+    newSoundex.resize(4, '0');
+    return newSoundex;
+}
+ 
+std::string generateSoundex(const std::string& name) {
+    if (name.empty()) {
+        return "";
+    }
+    std::string soundex(1, std::toupper(name[0]));
+    char prevCode = getSoundexCode(soundex[0]);
+    return updateSoundex(soundex, name, prevCode);
 }
